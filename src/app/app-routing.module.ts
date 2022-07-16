144c0fc4ff3c6d4ce6071fed3611870c3266ff94
Nisha Dudheliya <nishadudheliya@gmail.com>
2022-07-16 14:54:24 -0400

    landing page and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LandingComponent} from "./landing/landing.component";
+import {ContactComponent} from "./contact/contact.component";
+import {AboutComponent} from "./about/about.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component : LandingComponent},
+  {path: 'contact', component : ContactComponent},
+  {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import {HeaderModule} from "./shared/header/header.module";
+import { FooterComponent } from './shared/footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { ProductComponent } from './product/product.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    FooterComponent,
+    ContactComponent,
+    AboutComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
