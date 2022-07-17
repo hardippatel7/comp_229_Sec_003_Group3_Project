d10051da34eb68ca80722ed002baf47863b8929f
hardippatel7 <hardippatel7@gmail.com>
2022-07-16 20:16:13 -0400

    hardip commit 5 added routes

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -5,5 +5,7 @@ let productController = require('../controllers/product');
 
 router.get('/list', productController.productList);
 router.post('/add', productController.addProduct);
+router.put('/edit', productController.updateProduct);
+router.delete('/delete', productController.deleteProduct);
 
 module.exports = router;
