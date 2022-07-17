5a8e1caf68b2a96c8a8faa856627de8933c06038
Nisha Dudheliya <nishadudheliya@gmail.com>
2022-07-16 20:16:30 -0400

    Added service

---
+++ frontend/src/app/product.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  readonly baseUrl;
+  constructor(private http: HttpClient ) {
+    this.baseUrl = "http://localhost:3000/product";
+  }
+
+  getProductList(): Observable<any> {
+    return this.http.get(`${this.baseUrl}`);
+  }
+
+  createProduct(product: Object): Observable<Object> {
+    return this.http.post(`${this.baseUrl}`, product);
+  }
+
+  updateProduct(id: number, value: any): Observable<Object> {
+    return this.http.put(`${this.baseUrl}/${id}`, value);
+  }
+
+  deleteProduct(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${id}`, { responseType: 'text' });
+  }
+}
