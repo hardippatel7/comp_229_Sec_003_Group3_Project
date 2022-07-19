688a8a53acc5056e91444b6c872de0ab29661315
hardippatel7 <hardippatel7@gmail.com>
2022-07-18 21:24:23 -0400

    hardip commit 9 minor bug fix

--- backend/config/app.js
+++ backend/config/app.js
@@ -1,30 +1,30 @@
 
 //let createError = require('http-errors');
 let express = require('express');
+let cors = require('cors');
 let path = require('path');
-let cookieParser = require('cookie-parser');
-let compress = require('compression');
+//let cookieParser = require('cookie-parser');
 let bodyParser = require('body-parser');
-let methodOverride = require('method-override');
-let session = require('express-session');
+// let session = require('express-session');
 
 let app = express();
+app.use(cors());
+app.options('*', cors());
 
-app.use(session({
+/*app.use(session({
   saveUninitialized: true,
   resave: true,
   secret: "sessionSecret"
-}));
+}));*/
 
 
 let productsRouter = require('../routes/product');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
-app.use(express.static(path.join(__dirname, '../node_modules')));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, '../public')));
+// app.use(express.static(path.join(__dirname, '../node_modules')));
 
 app.use('/products', productsRouter);
-
 module.exports = app;
