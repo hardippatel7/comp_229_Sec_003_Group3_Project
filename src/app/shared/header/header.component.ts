04e06c3b4cc9087871d69ceb3476bcf9f451e6a5
Vinny Mariam Vinu <vinnyvinu98@gmail.com>
2022-08-04 23:39:47 -0400

    Release 2

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthGuardService } from 'src/app/auth/auth-guard.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
 
-  constructor() { }
+export class HeaderComponent implements OnInit, AfterViewInit {
+  isUserLogin: boolean = false;
+  constructor(public authService: AuthGuardService, public router: Router, public ref: ChangeDetectorRef) {
 
-  ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+
+  }
+
+  ngOnInit(): void {
+    this.authService.userSubject.subscribe((data) => {
+      this.isUserLogin = !data;
+    });
+  }
+
+  onLogout() {
+    this.authService.settoken("");
+    this.isUserLogin = false;
+  }
 }
